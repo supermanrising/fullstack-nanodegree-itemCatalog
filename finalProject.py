c47a9909a93c5d5c848f2377ce0317f8aba7303d
Ryan Vrba <ryan.vrba@gmail.com>
2015-12-26 12:19:45 -0800

    feat: Added functionality for JSON API endpoints

--- finalProject.py
+++ finalProject.py
@@ -119,6 +119,28 @@ def deleteMenuItem(restaurant_id, menu_id):
 		return render_template('delete-menu-item.html', item = item)
 
 
+#JSON stuff
+#done :)
+@app.route('/restaurants/JSON')
+def restaurantsJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(Restaurants=[i.serialize for i in restaurants])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=menuItem.serialize)
+
+
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
 	app.debug = True
